750601e5a180f89db84c3d516505179f7a466a0e
Huyen Lam <lamhonghuyen@gmail.com>
2019-12-05 02:32:22 +0700

    Add demo for H(x), Not(x), CNot(x)

--- demo1b.py
+++ demo1b.py
@@ -3,33 +3,40 @@
 from qiskit import (QuantumRegister, ClassicalRegister, QuantumCircuit)
 from qcomp import (qexec)
 
-# Task: Qubit |0>
+# Task: Put |0> into superposition
 qr = QuantumRegister(1) # Initailly = |0>
 cr = ClassicalRegister(1)
 circuit = QuantumCircuit(qr, cr)
+circuit.h(qr) # H(qr)
 circuit.measure(qr, cr)
 qexec(circuit)
 
-# Task: Qubit |1>
+# Task: Put QuBit in superposition back to previous state |0>
+qr = QuantumRegister(1) # Initailly = |0>
+cr = ClassicalRegister(1)
+circuit = QuantumCircuit(qr, cr)
+circuit.h(qr) # H(qr)
+circuit.h(qr) # H(qr)
+circuit.measure(qr, cr)
+qexec(circuit)
+
+# Task: Put |1> into superposition
 qr = QuantumRegister(1) # Initailly = |0>
 cr = ClassicalRegister(1)
 circuit = QuantumCircuit(qr, cr)
 circuit.x(qr) # Not(qr)
+circuit.h(qr) # H(qr)
 circuit.measure(qr, cr)
 qexec(circuit)
 
-# Task: Qubit |01>
-qr = QuantumRegister(2) # Initailly = |0>
-cr = ClassicalRegister(2)
+# Task: Put QuBit in superposition back to previous state |1>
+qr = QuantumRegister(1) # Initailly = |0>
+cr = ClassicalRegister(1)
 circuit = QuantumCircuit(qr, cr)
-circuit.x(qr[0]) # Not(qr)
+circuit.x(qr) # Not(qr)
+circuit.h(qr) # H(qr)
+circuit.h(qr) # H(qr)
 circuit.measure(qr, cr)
 qexec(circuit)
 
-# Task: Qubit |10>
-qr = QuantumRegister(2) # Initailly = |0>
-cr = ClassicalRegister(2)
-circuit = QuantumCircuit(qr, cr)
-circuit.x(qr[1]) # Not(qr)
-circuit.measure(qr, cr)
-qexec(circuit)
+# Application of double h(qr)? Restore quantum state?
--- demo1c.py
+++ demo1c.py
@@ -3,33 +3,38 @@
 from qiskit import (QuantumRegister, ClassicalRegister, QuantumCircuit)
 from qcomp import (qexec)
 
-# Task: Qubit |0>
-qr = QuantumRegister(1) # Initailly = |0>
-cr = ClassicalRegister(1)
-circuit = QuantumCircuit(qr, cr)
-circuit.measure(qr, cr)
-qexec(circuit)
-
-# Task: Qubit |1>
-qr = QuantumRegister(1) # Initailly = |0>
-cr = ClassicalRegister(1)
-circuit = QuantumCircuit(qr, cr)
-circuit.x(qr) # Not(qr)
-circuit.measure(qr, cr)
-qexec(circuit)
-
-# Task: Qubit |01>
+# Task: Control gate |00>
 qr = QuantumRegister(2) # Initailly = |0>
 cr = ClassicalRegister(2)
 circuit = QuantumCircuit(qr, cr)
-circuit.x(qr[0]) # Not(qr)
+circuit.cx(qr[0], qr[1]) # CNot(qr)
 circuit.measure(qr, cr)
 qexec(circuit)
 
-# Task: Qubit |10>
+# Task: Control gate |01>
 qr = QuantumRegister(2) # Initailly = |0>
 cr = ClassicalRegister(2)
 circuit = QuantumCircuit(qr, cr)
-circuit.x(qr[1]) # Not(qr)
+circuit.x(qr[1])
+circuit.cx(qr[0], qr[1]) # CNot(qr)
+circuit.measure(qr, cr)
+qexec(circuit)
+
+# Task: Control gate |10>
+qr = QuantumRegister(2) # Initailly = |0>
+cr = ClassicalRegister(2)
+circuit = QuantumCircuit(qr, cr)
+circuit.x(qr[0])
+circuit.cx(qr[0], qr[1]) # CNot(qr)
+circuit.measure(qr, cr)
+qexec(circuit)
+
+# Task: Control gate |10>
+qr = QuantumRegister(2) # Initailly = |0>
+cr = ClassicalRegister(2)
+circuit = QuantumCircuit(qr, cr)
+circuit.x(qr[0])
+circuit.x(qr[1])
+circuit.cx(qr[0], qr[1]) # CNot(qr)
 circuit.measure(qr, cr)
 qexec(circuit)
