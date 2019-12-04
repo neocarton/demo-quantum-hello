e1e88609baa843691d25b1cc4ce75ccc3f5399d3
Huyen Lam <lamhonghuyen@gmail.com>
2019-12-04 19:44:24 +0700

    Refactore code

--- Demo1.py
+++ Demo1.py
@@ -1,39 +1,15 @@
 # https://qiskit.org/documentation/getting_started.html
 
-import numpy as np
-from qiskit import(
-  QuantumCircuit,
-  execute,
-  Aer)
-from qiskit.visualization import plot_histogram
+from qiskit import (QuantumCircuit)
+from qcomp import (qexec)
 
 # Create a Quantum Circuit acting on the q register
 circuit = QuantumCircuit(2, 2)
-
-# Add a H gate on qubit 0
+# Add a H gate on qubit 1
 circuit.h(0)
-
 # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
 circuit.cx(0, 1)
-
 # Map the quantum measurement to the classical bits
 circuit.measure([0,1], [0,1])
-
-# Use Aer's qasm_simulator
-backend = Aer.get_backend('qasm_simulator')
-
-# Execute the circuit on the qasm backend
-job = execute(circuit, backend, shots=1000)
-
-# Grab results from the job
-result = job.result()
-
-# Returns counts
-counts = result.get_counts(circuit)
-print("\nTotal count for 00 and 11 are:", counts)
-
-# Draw the circuit
-print(circuit)
-
-# Draws a circuit with Matplotlib
-circuit.draw(output = "mpl")
+# Execute
+qexec(circuit)
---
+++ qcomp.py
@@ -0,0 +1,32 @@
+# Reference:
+# https://github.com/Qiskit/qiskit-community-tutorials/blob/master/hello_world/hello_zero.ipynb
+
+from qiskit import (IBMQ, providers, Aer, execute)
+
+def fetchBackend() :
+    # IBM Q backend
+    IBMQ.load_accounts()
+    backend = providers.ibmq.least_busy(IBMQ.backends(simulator = False))
+    print("Using the least busy device: ", backend.name())
+    return backend
+
+def getSimulator() :
+    # Aer's qasm_simulator
+    backend = Aer.get_backend('qasm_simulator')
+    print("Using: ", backend.name())
+    return backend
+
+def qexec(circuit, backend = None) :
+    if backend is None :
+        backend = defaultBackend
+    # Execute the circuit on the qasm backend
+    job = execute(circuit, backend, shots = 1000)
+    # Grab results from the job
+    result = job.result()
+    # Returns counts
+    counts = result.get_counts(circuit)
+    print("\nTotal count are:", counts)
+    # Draw the circuit
+    print(circuit)
+
+defaultBackend = getSimulator()
